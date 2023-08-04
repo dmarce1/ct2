8a91d846c9591860e3a2cb9135efdbcbeba85fd6
Dominic Marcello <dmarce504@gmail.com>
2023-08-04 09:52:42 -0500

    update

--- src/drift.cpp
+++ src/drift.cpp
@@ -7,9 +7,9 @@ void drift(float dt) {
 		const simd_f32 vx = *((simd_f32*) (&particle_vx(i)));
 		const simd_f32 vy = *((simd_f32*) (&particle_vy(i)));
 		const simd_f32 vz = *((simd_f32*) (&particle_vz(i)));
-		simd_fixed32 x = *((simd_fixed32*) (&particle_x(i)));
-		simd_fixed32 y = *((simd_fixed32*) (&particle_y(i)));
-		simd_fixed32 z = *((simd_fixed32*) (&particle_z(i)));
+		simd_fixed32& x = *((simd_fixed32*) (&particle_x(i)));
+		simd_fixed32& y = *((simd_fixed32*) (&particle_y(i)));
+		simd_fixed32& z = *((simd_fixed32*) (&particle_z(i)));
 		x = x + simd_fixed32(vx * dt);
 		y = y + simd_fixed32(vy * dt);
 		z = z + simd_fixed32(vz * dt);
